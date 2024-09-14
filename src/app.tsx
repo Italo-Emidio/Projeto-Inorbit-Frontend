a1b9ed529cc9f517ae0f1c341d3450c437463727
Italo-Emidio <italo.emidio7@gmail.com>
2024-09-14 18:05:48 -0300

    Foram criados todos os componentes da aplicação, tela de cadastro de nova meta e progresso

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,16 @@
-import { Plus } from "lucide-react";
-
-import logo from "./assets/logo-in-orbit.svg";
-import letsStart from "./assets/lets-start-illustration.svg";
-import { Button } from "./components/ui/button";
+import { Dialog } from "./components/ui/dialog";
+import { CreateGoal } from "./components/ui/create-goal";
+import { EmptyGoals } from "./components/ui/empty-goals";
+import { Summary } from "./components/ui/summary";
 
 export function App() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center gap-8">
-      <img src={logo} alt="in.orbit" />
-      <img src={letsStart} alt="in.orbit" />
-      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
-        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
-      </p>
+    <Dialog>
+      {/*<EmptyGoals /> */}
 
-      <Button>
-        <Plus className="size-4" />
-        Cadastrar meta
-      </Button>
-    </div>
+      <Summary />
+
+      <CreateGoal />
+    </Dialog>
   );
 }
